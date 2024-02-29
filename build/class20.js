5e4102a5edd33bc09355b8857d57761842b2d401
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2024-02-29 20:45:19 +0000

    class21

--- build/class20.js
+++ build/class20.js
@@ -1,11 +1,20 @@
 "use strict";
 class Conta {
-    constructor(number, owner) {
-        this.number = number;
+    constructor(owner) {
+        this.number = this.gerarNumberAccount();
         this.owner = owner;
+    }
+    gerarNumberAccount() {
+        return Math.floor(Math.random() * 10000) + 1;
     }
 }
 class AccountPF extends Conta {
+    constructor(cpf, owner) {
+        super(owner);
+        this.cpf = cpf;
+    }
 }
-const cont1 = new AccountPF(12, "Alfredo");
-console.log(cont1.number);
+class AccountPJ extends Conta {
+}
+const cont1 = new AccountPF(1, "Alfredo");
+console.log(cont1);
